3ad1e9aa0193f63c467d81806049006b1db1cec1
叶宁 <yen@mingyuanyun.com>
2016-12-28 14:43:38 +0800

    测试

---
+++ api/test.js
@@ -0,0 +1,211 @@
+{
+    "swagger": "2.0",
+    "info": {
+        "contact": {
+            
+        },
+        "title": "测试222222",
+        "version": "1.0.0",
+        "description": "wsdds"
+    },
+    "basePath": "/",
+    "tags": [
+        
+    ],
+    "schemes": [
+        "http"
+    ],
+    "securityDefinitions": {
+        
+    },
+    "definitions": {
+        
+    },
+    "paths": {
+        "/api/team": {
+            "post": {
+                "tags": [
+                    "apicloud"
+                ],
+                "summary": "创建团队",
+                "description": "创建团队",
+                "operationId": "",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "deprecated": false,
+                "parameters": [
+                    {
+                        "name": "id",
+                        "in": "formData",
+                        "description": "ID值",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "name",
+                        "in": "formData",
+                        "description": "ID值",
+                        "required": true,
+                        "type": "string"
+                    },
+                    {
+                        "name": "members",
+                        "in": "formData",
+                        "description": "ID值",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "owner",
+                        "in": "formData",
+                        "description": "ID值",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "description",
+                        "in": "formData",
+                        "description": "ID值",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "id",
+                        "in": "query",
+                        "description": "11",
+                        "required": false,
+                        "default": "",
+                        "type": "string"
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "无",
+                        "schema": {
+                            "properties": {
+                                "msg": {
+                                    "description": "ID值",
+                                    "type": "string"
+                                },
+                                "result": {
+                                    "description": "ID值",
+                                    "type": "string"
+                                }
+                            },
+                            "type": "object"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}11{
+    "swagger": "2.0",
+    "info": {
+        "contact": {
+            
+        },
+        "title": "测试222222",
+        "version": "1.0.0",
+        "description": "wsdds"
+    },
+    "basePath": "/",
+    "tags": [
+        
+    ],
+    "schemes": [
+        "http"
+    ],
+    "securityDefinitions": {
+        
+    },
+    "definitions": {
+        
+    },
+    "paths": {
+        "/api/team": {
+            "post": {
+                "tags": [
+                    "apicloud"
+                ],
+                "summary": "创建团队",
+                "description": "创建团队",
+                "operationId": "",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "deprecated": false,
+                "parameters": [
+                    {
+                        "name": "id",
+                        "in": "formData",
+                        "description": "ID值",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "name",
+                        "in": "formData",
+                        "description": "ID值",
+                        "required": true,
+                        "type": "string"
+                    },
+                    {
+                        "name": "members",
+                        "in": "formData",
+                        "description": "ID值",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "owner",
+                        "in": "formData",
+                        "description": "ID值",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "description",
+                        "in": "formData",
+                        "description": "ID值",
+                        "required": false,
+                        "type": "string"
+                    },
+                    {
+                        "name": "id",
+                        "in": "query",
+                        "description": "11",
+                        "required": false,
+                        "default": "",
+                        "type": "string"
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "无",
+                        "schema": {
+                            "properties": {
+                                "msg": {
+                                    "description": "ID值",
+                                    "type": "string"
+                                },
+                                "result": {
+                                    "description": "ID值",
+                                    "type": "string"
+                                }
+                            },
+                            "type": "object"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}FORMAT: 1A
